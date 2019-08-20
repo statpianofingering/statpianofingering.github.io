f89b25f459d81aaad43f3699a16e647d1b7dd352
statpianofingering <49031163+statpianofingering@users.noreply.github.com>
2019-08-20 21:28:50 +0900

    Add files via upload

--- PianoRoll.js
+++ PianoRoll.js
@@ -63,63 +63,66 @@ class PianoRoll{
 	ReadMIDIFile(file){
 		this.Clear();
 		var midi=new Midi();
-		var tmp=midi.ReadFile(file);
 
-		console.log(midi.evts.length);
+		return midi.ReadFile(file).then(() => {
+// 			console.log("- In PianoRoll.ReadMidiFile(): midi.ReadFile(file).then();");
+// 			console.log(midi.evts.length);
 
-		var onPosition=[];
-		for(var i=0;i<16;i+=1){onPosition[i]=[];for(var j=0;j<128;j+=1){onPosition[i][j]=-1;}}//endfor i,j
-		var evt=new PianoRollEvt();
-		var curChan;
+			var onPosition=[];
+			for(var i=0;i<16;i+=1){onPosition[i]=[];for(var j=0;j<128;j+=1){onPosition[i][j]=-1;}}//endfor i,j
+			var evt=new PianoRollEvt();
+			var curChan;
 
-		for(var n=0,len=midi.evts.length;n<len;n+=1){
+			for(var n=0,len=midi.evts.length;n<len;n+=1){
 
-			if(midi.evts[n].mes[0]>=128 && midi.evts[n].mes[0]<160){//note-on or note-off event
-				curChan=midi.evts[n].mes[0]%16;
-				if(midi.evts[n].mes[0]>=144 && midi.evts[n].mes[2]>0){//note-on
-					if(onPosition[curChan][midi.evts[n].mes[1]]>=0){
-						console.log('Warning: (Double) note-on event before a note-off event '+PitchToSitch(midi.evts[n].mes[1]));
+				if(midi.evts[n].mes[0]>=128 && midi.evts[n].mes[0]<160){//note-on or note-off event
+					curChan=midi.evts[n].mes[0]%16;
+					if(midi.evts[n].mes[0]>=144 && midi.evts[n].mes[2]>0){//note-on
+						if(onPosition[curChan][midi.evts[n].mes[1]]>=0){
+							console.log('Warning: (Double) note-on event before a note-off event '+PitchToSitch(midi.evts[n].mes[1]));
+							this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offtime=midi.evts[n].time;
+							this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offvel=-1;
+						}//endif
+						onPosition[curChan][midi.evts[n].mes[1]]=this.evts.length;
+						evt.channel=curChan;
+						evt.sitch=PitchToSitch(midi.evts[n].mes[1]);
+						evt.pitch=midi.evts[n].mes[1];
+						evt.onvel=midi.evts[n].mes[2];
+						evt.offvel=0;
+						evt.ontime=midi.evts[n].time;
+						evt.offtime=evt.ontime+0.1;
+						var evt_=JSON.stringify(evt);
+						evt_=JSON.parse(evt_);
+						this.evts.push(evt_);
+					}else{//note-off
+						if(onPosition[curChan][midi.evts[n].mes[1]]<0){
+							console.log('Warning: Note-off event before a note-on event '+PitchToSitch(midi.evts[n].mes[1])+"\t"+midi.evts[n].time);
+							continue;
+						}//endif
 						this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offtime=midi.evts[n].time;
-						this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offvel=-1;
+						if(midi.evts[n].mes[2]>0){
+							this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offvel=midi.evts[n].mes[2];
+						}else{
+							this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offvel=80;
+						}//endif
+						onPosition[curChan][midi.evts[n].mes[1]]=-1;
 					}//endif
-					onPosition[curChan][midi.evts[n].mes[1]]=this.evts.length;
-					evt.channel=curChan;
-					evt.sitch=PitchToSitch(midi.evts[n].mes[1]);
-					evt.pitch=midi.evts[n].mes[1];
-					evt.onvel=midi.evts[n].mes[2];
-					evt.offvel=0;
-					evt.ontime=midi.evts[n].time;
-					evt.offtime=evt.ontime+0.1;
-					var evt_=JSON.stringify(evt);
-					evt_=JSON.parse(evt_);
-					this.evts.push(evt_);
-				}else{//note-off
-					if(onPosition[curChan][midi.evts[n].mes[1]]<0){
-						console.log('Warning: Note-off event before a note-on event '+PitchToSitch(midi.evts[n].mes[1])+"\t"+midi.evts[n].time);
-						continue;
-					}//endif
-					this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offtime=midi.evts[n].time;
-					if(midi.evts[n].mes[2]>0){
-						this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offvel=midi.evts[n].mes[2];
-					}else{
-						this.evts[onPosition[curChan][midi.evts[n].mes[1]]].offvel=80;
-					}//endif
-					onPosition[curChan][midi.evts[n].mes[1]]=-1;
 				}//endif
-			}//endif
-		}//endfor n
 
-		for(var i=0;i<16;i+=1)for(var j=0;j<128;j+=1){
-			if(onPosition[i][j]>=0){
-				console.log('Error: Note without a note-off event');
-				console.log('ontime channel sitch : '+this.evts[onPosition[i][j]].ontime+"\t"+this.evts[onPosition[i][j]].channel+"\t"+this.evts[onPosition[i][j]].sitch);
-				return;
-			}//endif
-		}//endfor i,j
+			}//endfor n
+			for(var i=0;i<16;i+=1)for(var j=0;j<128;j+=1){
+				if(onPosition[i][j]>=0){
+					console.log('Error: Note without a note-off event');
+					console.log('ontime channel sitch : '+this.evts[onPosition[i][j]].ontime+"\t"+this.evts[onPosition[i][j]].channel+"\t"+this.evts[onPosition[i][j]].sitch);
+					return;
+				}//endif
+			}//endfor i,j
 
-		for(var n=0,len=this.evts.length;n<len;n+=1){
-			this.evts[n].ID=n;
-		}//endfor n
+			for(var n=0,len=this.evts.length;n<len;n+=1){
+				this.evts[n].ID=n;
+			}//endfor n
+
+		});//return midi.then(ccallback);
 
 //console.log(this.evts.length);
 
